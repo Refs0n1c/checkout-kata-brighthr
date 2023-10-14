c1dc86b28ab91b60a222c62bc6ee71e289d6d36c
Daniel <daniel.refson2306@gmail.com>
2023-10-14 14:33:57 +0100

    added test for gettotalprice

--- Checkout.Tests/CheckoutServiceTests.cs
+++ Checkout.Tests/CheckoutServiceTests.cs
@@ -1,9 +1,11 @@
+using Checkout.Data;
 using Checkout.Features;
 
 namespace Checkout.Tests;
 
 public class CheckoutServiceTests
 {
+    private const int priceA = 50;
     private ICheckoutService _checkout;
 
     [SetUp]
@@ -50,5 +52,18 @@ public class CheckoutServiceTests
     public void Scan_ThrowsException_When_ItemNotFound()
     {
         Assert.Throws<Exception>(() => _checkout.Scan("Z"));
+    }
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    public void GetTotalPrice_ShouldReturnCorrectPrice(int itemsQty)
+    {
+        for (int i = 0; i < itemsQty; i++)
+        {
+            _checkout.Scan("A");
+        }
+        Assert.That(_checkout.GetTotalPrice(), Is.EqualTo(itemsQty * priceA));
     }
 }
